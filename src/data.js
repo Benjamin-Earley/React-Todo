8914821a2ab52646ea207a46dc1f4600f9928bc1
Benjamin Earley <benjaminearley@MacBook-Pro.local>
2020-04-08 20:01:23 -0400

    Initialized project, created data array, laid structure of Todo.js.

---
+++ src/data.js
@@ -0,0 +1,22 @@
+export const todoData = [
+    {
+        task: "Hit the gym",
+        id: Date.now(),
+        completed: false
+    },
+    {
+        task: "Get groceries",
+        id: 1,
+        completed: false
+    },
+    {
+        task: "Do laundry",
+        id: 2,
+        completed: false
+    },
+    {
+        task: "Study",
+        id: 3,
+        completed: false
+    }
+]
